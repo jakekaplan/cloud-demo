a72be01f4c028454b03de9ecd18da5f6a41493fd
jake@prefect.io <jake@prefect.io>
2025-02-11 01:58:25 -0500

    commit

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
+import prefect
+import pandas
+import os
+
+@flow
 def hello_world():
     print("Hello world!")
+
+@flow
+def hello_pandas(name: str):
+    print(f"hello {name}!")
+    print("pandas version", pandas.__version)
+    print(os.environ.get("CLOUD_ENV"))
+    
