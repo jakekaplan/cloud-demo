0c461db56cc37efce5b5eedd502ffb05561d808b
jake@prefect.io <jake@prefect.io>
2025-02-11 11:43:18 -0500

    add other file

--- hello.py
+++ hello.py
@@ -1,11 +1,3 @@
-import pandas
-import os
-
 def hello_world():
-    print("Hello world!")
+    print("hello world!")
 
-def hello_pandas(name: str):
-    print(f"hello {name}!")
-    print("pandas version", pandas.__version__)
-    print(os.environ.get("PANDA"))
-    
