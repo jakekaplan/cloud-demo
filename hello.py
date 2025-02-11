d2222e1dd1c5e89be869b4dc9a806868e359ee8d
jake@prefect.io <jake@prefect.io>
2025-02-11 02:16:35 -0500

    remember to commit

--- hello.py
+++ hello.py
@@ -7,5 +7,5 @@ def hello_world():
 def hello_pandas(name: str):
     print(f"hello {name}!")
     print("pandas version", pandas.__version__)
-    print(os.environ.get("CLOUD_ENV"))
+    print(os.environ.get("PANDA"))
     
